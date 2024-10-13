1e63d61f71abdb413b9a48e01a7a373047b4fe25
vadim <vadim.polinevskiy06@mail.ru>
2024-10-13 11:45:16 +0300

    лаб 5 коммит 2

--- lab5/Project1/sourse.cpp
+++ lab5/Project1/sourse.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <ctime>
 #include <vector>
+#include <cstring>
 using namespace std;
 
 class graph
@@ -116,90 +117,22 @@ public:
         }
         for (int i = 0; i < number_of_vertices; i++) {
             for (int j = 0; j < array_edges.size()/2; j= j + 2 ) {
-                if (i == array_edges[j] || (i == array_edges[j + 1] && array_edges[j + 1] != NULL)) {
+                if (i == array_edges[j]) {
                     identity_matrix[i][j] = 1;
+                }
+                if (i == array_edges[j + 1]){
+                    identity_matrix[i][j+1] = 1;
                 }
             }
         }
     }
 private:
     vector <int> array_edges;
-    //int* array_edges{ NULL };
     int number_of_vertices {0};
     int** array = { NULL };
     int** identity_matrix{ NULL };
 };
-//void determining_the_number_of_vertices_of_the_graph(int** array, int number_of_vertices) {
-//    int count = 0;
-//    for (int i = 0; i < number_of_vertices; i++) {
-//        for (int j = i+1; j < number_of_vertices; j++) {
-//            if (array[i][j] == 1) count++;
-//        }
-//    }
-//    cout <<"Count:  "<< count << endl;
-//    return;
-//}
-//void print_array(int **array, int number_of_vertices){
-//    cout << "----------------------\n" << endl;
-//    for (int i = 0; i < number_of_vertices; i++){
-//        for (int j = 0; j < number_of_vertices; j++){
-//            cout << array[i][j] << "  ";
-//        }
-//        cout << "\n"<< endl;
-//    }
-//    return;
-//} 
-//void create_random_adjacency_matrix(int **array, int number_of_vertices){
-//    for (int i = 0; i < number_of_vertices; i++){
-//        for (int j = i+1; j < number_of_vertices; j++){
-//            array[i][j] = rand() % 2 - 0;
-//            if (array[i][j] == 1) array[j][i] = 1;
-//        }
-//    }
-//}
-//void count_vertices(int** array, int number_of_vertices) {
-//    int temp_count{ 0 };
-//    int i = 0; int j = 0;
-//    while (i < number_of_vertices) {
-//        while(j < number_of_vertices) {  
-//            if (array[i][j] == 1) {
-//                temp_count++;
-//            }
-//            j++;
-//        }
-//        if (temp_count == 1) {
-//            cout << "end vertex : { " << i << " }" << endl;
-//        }
-//        if (temp_count == 0) {
-//            cout << "isolated vertex :  { " << i << " }" << endl;
-//        }
-//        if (temp_count == number_of_vertices - 1) {
-//            cout << "dominant vertex :  { " << i << " }" << endl;
-//        }
-//        temp_count = 0;
-//        i++;
-//        j = 0;
-//
-//    }
-//    /*for (int i = 0; i < number_of_vertices; i++) {
-//        for (int j = i+1; j < number_of_vertices; j++) {
-//            if (array[i][j] == 1) {
-//                temp_count++;
-//            }
-//        }
-//        if (temp_count == 1) {
-//            cout << "end vertex : { " << i << " } { " << j << " }" << endl;
-//        }
-//        if (temp_count == 0) {
-//            cout << "isolated vertex : { " << i << " } { " << j << " }" << endl;
-//        }
-//        if (temp_count == number_of_vertices - 1) {
-//            cout << "dominant vertex : { " << i << " } { " << j << " }" << endl;
-//        }
-//        temp_count = 0;
-//    }*/
-//
-//}
+
 void task_1(){
     graph G;
     G.Set_number_of_vertices();
@@ -215,7 +148,6 @@ void task_1(){
 }
 void task_2() {
     int* a = NULL;
-    //a = (int*)malloc(number_of_verticesof(int));
     int array_of_edges;
 }
 int main(){
